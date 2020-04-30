b3f599b4efb63a9b7ffe07773ca23f47cfa1b1dd
CJ <xf97719n@pace.edu>
2020-04-29 21:30:23 -0400

    Task 4

--- lab9/task4/two_way_assoc.c
+++ lab9/task4/two_way_assoc.c
@@ -5,7 +5,7 @@ int mru[4] = {1,1,1,1};
 
 int main( )
 
-{   int j,set,t;
+{   int i, set, t;
     int addr;
     int hits, accesses;
     FILE *fp;
@@ -13,117 +13,50 @@ int main( )
     fp = fopen("trace.txt", "r");
     hits = 0;
     accesses = 0;
-    while (fscanf(fp, "%x", &addr) > 0) {
+    while (fscanf(fp, "%x", &addr) > 0) 
+    {
 
        accesses += 1;
        printf("\n%3d: 0x%08x ", accesses, addr);
 
-        set = (addr >> 2) & 1; //determines set
-        t = addr | 0x1f;
+        set = (addr >> 2) & 3; //determines set
+        t = addr | 0xf;
 
-            
-
-            if(set == 0)
-            { 
-                
-                int count = 0;
-
-                for(j = 0; j < 4; j++)
-                {
-                    //hit
-                    
-                    
-                    if(tag[0][j] == t)
-                    {
-                        hits+=1;
-                        count++;
-                        //mru[j] = 1;
-                        printf("Hit%d" , j );
-                        printf("\n");
-                        break;
-                    }
-
-                    else
-                    {
-                        printf("MISS");
-                        tag[0][count] = t;
-                    }
-
-                }
-                    
-
-
-                   
-                    
-                
-
-                    
-                        
-
+        for (i = 0; i < 2; i++ )
+        {
+            if (tag[i][set] == t)
+            {
+                printf("HIT");
+                hits += 1;
+                mru[set] = i;
+                break;
             }
-
-        
-
-           else if(set == 1)
-            { 
-                
-                int count = 0;
-
-                for(j = 0; j < 4; j++)
-                {
-                    //hit
-                    
-                    
-                    if(tag[1][j] == t)
-                    {
-                        hits+=1;
-                        count++;
-                        //mru[j] = 1;
-                        printf("Hit%d" , j );
-                        printf("\n");
-                        break;
-                    }
-
-                    else
-                    {
+            else if (tag[0][set] != t && tag[1][set] != t)
+            {
                         printf("MISS");
-                        tag[1][count] = t;
-                    }
-
-                }
-                    
-
-
-                     
-                    
-                
-
-                    
-                        
-
+                        tag[!mru[set]][set] = t;
+                        mru[set] = !mru[set];
+                        break;
             }
-            
-            
-            
-            
-        
-
-        /*if (i == 8) {
-            // allocate entry 
-            printf("Miss ");
-            i = mru[7];
-            tag[i] = addr;
-            mruUpdate(i);
         }
-        for (i = 0; i < 8; i++)
-            printf("0x%08x ", tag[i]);
-        for (i = 0; i < 8; i++)
-            printf("%d ", mru[i]);
-        printf("\n"); */
-
+    for (i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf(" 0x%08x", tag[i][j]);
+        }
         
     }
-    printf("Hits = %d" , hits); 
+    for (int j = 0; j < 4; j++)
+     {
+         printf(" %d", mru[j]);
+     }
+            
+    }
+
+
+
+    printf("\nHits = %d" , hits); 
     printf("Hits = %d, Accesses = %d, Hit ratio = %f\n", hits, accesses, ((float)hits)/accesses);
     fclose(fp);
 }
